e64d93e24595d460b2fc761e5e24e455e4b3dbe2
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-25 00:50:19 +0300

    completed my assigment

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Code your solutions in this file
+const g = ["teddy bear", "drone", "doll"];
+
+function wrapGift(gift) {
+  console.log(`Wrapped ${gift} and added a bow!`);
+}
+wrapGift(gifts[0]);
+wrapGift(gifts[1]);
+wrapGift(gifts[2]);
+for ([initialization]; [condition]; [iteration]) {
+    [loop ,body]
+    for (let age = 30; age < 40; age++) {}
+        console.log(`I'm ${age} years old. Happy birthday to me!`);}
+    debugger; 
+    const gif = ["teddy bear", "drone", "doll"];
+
+    function wrapGifts(gifts) {
+      for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+      }
+    
+      return gifts;
+    }
+    
+    wrapGifts(gifts);
+    writeCards(["Charlie", "Samip", "Ali"], "birthday");
+    [
+        "Thank you, Charlie, for the wonderful birthday gift!",
+        "Thank you, Samip, for the wonderful birthday gift!",
+        "Thank you, Ali, for the wonderful birthday gift!",
+        w ([condition])
+        
+            [loop ,body]
+    ]
+    const gifts = ["teddy bear", "drone", "doll"];
+
+    function wrapGifts(gifts) {
+      let i = 0; // the initialization moves OUTSIDE the body of the loop!
+      while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++; // the iteration moves INSIDE the body of the loop!
+      }
+    
+      return gifts;
+    }
+    
+    wrapGifts(gifts);
+    // LOG: Wrapped teddy bear and added a bow!
+    // LOG: Wrapped drone and added a bow!
+    // LOG: Wrapped doll and added a bow!
+    // => ["teddy bear", "drone", "doll"]
+    function plantGarden() {
+        let keepWorking = true;
+        while (keepWorking) {
+          chooseSeedLocation();
+          plantSeed();
+          waterSeed();
+          keepWorking = checkForMoreSeeds();
+        }
+      }
+      let countup = 0;
+      while (countup < 10) {
+        console.log(countup++);
+      }
+      for (let countup = 0; countup < 10; countup++) {
+        console.log(countup);
+      }
+
+
